11ca5f8d8fa100b070ba5a7a4ae3b1cfa3da6bad
OdinPT <leonardo.almeidavieira@gmail.com>
2020-06-01 09:27:34 +0100

    print all data from pdf file

--- ReturnDataFromPDF/Program.cs
+++ ReturnDataFromPDF/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.IO;
 
 namespace ReturnDataFromPDF
 {
     class Program
     {
+
+        public static string GetTextFromAllPages(String pdfPath)
+        {
+            PdfReader reader = new PdfReader(pdfPath);
+
+            StringWriter output = new StringWriter();
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
+
+            return output.ToString();
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+   
+
+            //directory scan file
+            string target = @"C:\Users\OdinPT\Desktop\";
+            //string target = @" ";
+            //file name
+            string file = @"lv.pdf";
+            string teste = @"comandos.txt";
+
+
+            FileStream fileStream = new FileStream(target + file, FileMode.Open);
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = reader.ReadLine();
+                Console.WriteLine(line.ToString());
+            }
+
+
+            Console.WriteLine(GetTextFromAllPages(target + file));
+
         }
     }
 }
