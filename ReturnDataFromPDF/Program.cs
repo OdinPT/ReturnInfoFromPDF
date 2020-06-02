dd66a3ee1fcfe8d34651b5d0a4e39d76d28be333
OdinPT <leonardo.almeidavieira@gmail.com>
2020-06-02 23:40:53 +0100

    a mostrar as 4 linhas iniciais no terminal

--- ReturnDataFromPDF/Program.cs
+++ ReturnDataFromPDF/Program.cs
@@ -2,33 +2,23 @@
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using System;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ReturnDataFromPDF
 {
     class Program
     {
-        
-        public static string GetTextFromAllPages(String pdfPath)
-        {
-            PdfReader reader = new PdfReader(pdfPath);
 
-            StringWriter output = new StringWriter();
-
-            for (int i = 1; i <= reader.NumberOfPages; i++)
-                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new LocationTextExtractionStrategy()));
-          ;
-            return output.ToString();
-        }
-
-        public static string ReadtextwithinPDF(string fileName)
+       public static string ReadDATAFromPDF(string fileName)
         {
             StringBuilder sb = new StringBuilder();
             if (File.Exists(fileName))
             {
                 PdfReader reader = new PdfReader(fileName);
-                Rectangle rect = new Rectangle(0, 0, 415, 745);
+                Rectangle rect = new Rectangle(0, 0, 415, 775);
 
                 RenderFilter[] filter = { new RegionTextRenderFilter(rect) };
                 ITextExtractionStrategy strategy;
@@ -39,41 +29,47 @@ namespace ReturnDataFromPDF
                     sb.AppendLine(PdfTextExtractor.GetTextFromPage(reader, i, strategy));
                 }
             }
-
-            //string path = @"C:\Users\OdinPT\Desktop\hereIAm.txt";
-            String path = @"..\..\..\teste.txt";
-
-            if (!File.Exists(path))
-            {
-                // Create a file to write to.
-                using (StreamWriter sw = File.CreateText(path))
-                {
-                    sw.WriteLine(sb.ToString());
-                   
-                }
-            }
+            MakeFile(sb);
 
             return sb.ToString();
         }
 
-//função que apaga as primeiras 15 strings
-
+        public static void MakeFile(StringBuilder s)
+        {
+            String path = @"..\..\..\temp.txt";
+            // Create a file to write to.
+            using StreamWriter sw = File.CreateText(path);
+            sw.WriteLine(s.ToString());
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             //directory scan file
             string target = @"C:\Users\OdinPT\Desktop\";
-            //file name 
+
             string file = @"lv.pdf";
-
             //string file = @"cv_eng.pdf";
-            string teste = @"teste.txt";
+            
+
+            //print in terminal and make txt file
+            Console.WriteLine(ReadDATAFromPDF(target + file));
+
+            // está a imprimire e a salvar toda a infoirmação do cv no txt 
+
+            String path = @"..\..\..\temp.txt";
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            Console.WriteLine(lines.Length + "\n");
 
 
-            Console.WriteLine(ReadtextwithinPDF(target + file));
+            for (int i = 0; i <=4; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+
 
         }
     }
 }
+//Só nome, email e numero do telemóvel
