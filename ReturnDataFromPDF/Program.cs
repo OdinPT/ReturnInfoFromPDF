ac801dc302416ff6eda2985c2e786aeeced77d5c
OdinPT <leonardo.almeidavieira@gmail.com>
2020-06-02 16:08:51 +0100

    savind data in my desktop txt file

--- ReturnDataFromPDF/Program.cs
+++ ReturnDataFromPDF/Program.cs
@@ -1,13 +1,15 @@
-﻿using iTextSharp.text.pdf;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using System;
 using System.IO;
+using System.Text;
 
 namespace ReturnDataFromPDF
 {
     class Program
     {
-
+        
         public static string GetTextFromAllPages(String pdfPath)
         {
             PdfReader reader = new PdfReader(pdfPath);
@@ -15,34 +17,60 @@ namespace ReturnDataFromPDF
             StringWriter output = new StringWriter();
 
             for (int i = 1; i <= reader.NumberOfPages; i++)
-                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new SimpleTextExtractionStrategy()));
-
+                output.WriteLine(PdfTextExtractor.GetTextFromPage(reader, i, new LocationTextExtractionStrategy()));
+          ;
             return output.ToString();
         }
+
+        public static string ReadtextwithinPDF(string fileName)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (File.Exists(fileName))
+            {
+                PdfReader reader = new PdfReader(fileName);
+                Rectangle rect = new Rectangle(0, 0, 415, 745);
+
+                RenderFilter[] filter = { new RegionTextRenderFilter(rect) };
+                ITextExtractionStrategy strategy;
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
+                    sb.AppendLine(PdfTextExtractor.GetTextFromPage(reader, i, strategy));
+                }
+            }
+
+            string path = @"C:\Users\OdinPT\Desktop\hereIAm.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(sb.ToString());
+                   
+                }
+            }
+
+            return sb.ToString();
+        }
+
+//função que apaga as primeiras 15 strings
 
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-   
 
             //directory scan file
             string target = @"C:\Users\OdinPT\Desktop\";
-            //string target = @" ";
-            //file name
+            //file name 
             string file = @"lv.pdf";
-            string teste = @"comandos.txt";
+
+            //string file = @"cv_eng.pdf";
+            string teste = @"teste.txt";
 
 
-            FileStream fileStream = new FileStream(target + file, FileMode.Open);
-            using (StreamReader reader = new StreamReader(fileStream))
-            {
-                string line = reader.ReadLine();
-                Console.WriteLine(line.ToString());
-            }
-
-
-            Console.WriteLine(GetTextFromAllPages(target + file));
+            Console.WriteLine(ReadtextwithinPDF(target + file));
 
         }
     }
