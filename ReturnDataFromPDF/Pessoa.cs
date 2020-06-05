c9408c130089e9f91de43f58d9bfb9c3341f5876
OdinPT <leonardo.almeidavieira@gmail.com>
2020-06-05 12:11:46 +0100

    a colocar na classe os campos nome, email e numero de telemóvel

---
+++ ReturnDataFromPDF/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReturnDataFromPDF
+{
+      class Pessoa
+    {
+        
+        public string Nome { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+
+        public Pessoa(string nome, string contact, string email)
+        {
+            Nome = nome;
+            Contact = contact;
+            Email = email;
+
+            Console.WriteLine("nome " + nome);
+            Console.WriteLine("Contacto " + contact);
+            Console.WriteLine("email " + email);
+        }
+
+        public override string ToString()
+        {
+            string temp = " ";
+            temp += Nome;
+            temp += Contact;
+            temp += Email;
+            return temp;
+        }
+    }
+}
--- ReturnDataFromPDF/Program.cs
+++ ReturnDataFromPDF/Program.cs
@@ -1,18 +1,21 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using Org.BouncyCastle.Utilities.Collections;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
 
+
 namespace ReturnDataFromPDF
 {
     class Program
     {
-
-       public static string ReadDATAFromPDF(string fileName)
+       
+        public static string ReadDATAFromPDF(string fileName, string path)
         {
             StringBuilder sb = new StringBuilder();
             if (File.Exists(fileName))
@@ -27,49 +30,91 @@ namespace ReturnDataFromPDF
                 {
                     strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
                     sb.AppendLine(PdfTextExtractor.GetTextFromPage(reader, i, strategy));
+  
                 }
             }
-            MakeFile(sb);
-
+            MakeTempFile(sb, path); 
             return sb.ToString();
         }
 
-        public static void MakeFile(StringBuilder s)
+        //gera ficheiro com todo o texto do pdf
+        public static void MakeTempFile(StringBuilder s, string path)
         {
-            String path = @"..\..\..\temp.txt";
             // Create a file to write to.
             using StreamWriter sw = File.CreateText(path);
+
             sw.WriteLine(s.ToString());
+            
         }
 
-        static void Main(string[] args)
+        public static void FirstLines(string path, string end)
         {
-            Console.WriteLine("Hello World!");
-            //directory scan file
-            string target = @"C:\Users\OdinPT\Desktop\";
+            List<Pessoa> pessoas;
 
-            string file = @"lv.pdf";
+            string[] lines = System.IO.File.ReadAllLines(path);
+            using StreamWriter sw = File.CreateText(end);
+
+            string word = lines[0];
+
+            // retira as 2 palavras antes do nome
+            int x = word.IndexOf(" ") + 1;
+            string str = word.Substring(x);
+            int z = str.IndexOf(" ") + 1;
+            string nome = str.Substring(z);
+
+            // escreve o nome no ficheiro
+            sw.WriteLine(nome);
+
+            // add  values to class pessoa
+            pessoas   = new List<Pessoa>();
+            Pessoa emp3 = new Pessoa(nome, lines[2], lines[4]);
+            pessoas.Add(emp3);
+            
+        }
+
+        //Só nome, email e numero do telemóvel
+        public static void Main(string[] args)
+        {
+
+            List<Pessoa> pessoas;
+
+            //Console.WriteLine("Hello World!");
+
+            //directory scan file and pdf to read
+            string target = @"C:\Users\OdinPT\Desktop\";
+            string file = @"joana.pdf";
             //string file = @"cv_eng.pdf";
+
+
+            //directory file with  all data
+            String path = @"..\..\..\temp.txt";
+
+            //directory file with  name. number and email from cv
+            String end = @"..\..\..\DataFromPDF.txt";
+
+            //read from pdf and write in txt temp file
+            ReadDATAFromPDF(target + file, path);
             
 
-            //print in terminal and make txt file
-            Console.WriteLine(ReadDATAFromPDF(target + file));
+            FirstLines(path, end);
 
-            // está a imprimire e a salvar toda a infoirmação do cv no txt 
+            //File.Delete(path);
 
-            String path = @"..\..\..\temp.txt";
-            string[] lines = System.IO.File.ReadAllLines(path);
+            //pessoas = new List<Pessoa>();
 
-            Console.WriteLine(lines.Length + "\n");
+            //for (int i = 2; i <= 4; i++)
+            //{
+            //    Console.WriteLine("\n\n");
+            //    Pessoa emp3 = new Pessoa(" Joã XX ", "1111111111", "1222222");
+            //    //pessoas.Add(emp3);
+
+            //    Console.WriteLine(emp3.ToString());
 
 
-            for (int i = 0; i <=4; i++)
-            {
-                Console.WriteLine(lines[i]);
-            }
+            //}
 
 
         }
     }
 }
-//Só nome, email e numero do telemóvel
+
